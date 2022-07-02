8a45dc44391499e212805d0638f8aa16863238ed
Blaine Ehrhart <blaine@nextleveltiming.com>
2022-07-01 19:00:51 -0700

    chore: cleanup implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import SocketClient from './SocketClient';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [transponder, setTransponder] = useState('123');
+    const [apiKey, setApiKey] = useState(SocketClient.getApiKey());
+
+    return (
+        <div className="app">
+            <h1>Neon Timing Client Device Demo</h1>
+
+            <h2>Connection Settings</h2>
+            <div>
+                <label>API Key</label><br/>
+                <input type="text" name="token" onChange={e => {
+                    setApiKey(e.target.value);
+                    SocketClient.setApiKey(e.target.value);
+                }} value={apiKey} style={{width: '16rem'}}/>
+            </div>
+
+            <h2>Transponder Passed Gate Test</h2>
+            <p><button onClick={() => SocketClient.newGateEvent(transponder)}>New Lap</button></p>
+            <div>
+                <p>
+                    <label>Transponder</label><br/>
+                    <input name="timeout" onChange={e => setTransponder(e.target.value)} value={transponder}/>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/SocketClient.js
@@ -0,0 +1,106 @@
+import {io} from "socket.io-client";
+import debounce from "lodash.debounce";
+
+class SocketClient {
+    constructor() {
+        this.socket = null;
+        this.apiKey = this.getApiKey();
+        this.fakeDeviceTimeOffset = Date.now();
+        this.hostSupportedEvents = [];
+        this.clientSupportedEvents = ['race','flag'];
+        this.did = window.localStorage.getItem('nt-did');
+        if (!this.did) {
+            this.did = Math.random().toString(36).slice(2);
+            window.localStorage.setItem('nt-did', this.did);
+        }
+
+        this.debounceAttemptConnection = debounce(() => {
+            this.attemptConnection();
+        }, 2000);
+
+        this.attemptConnection();
+    }
+
+    attemptConnection() {
+        if (this.socket) {
+            console.log('Stopping previous connection attempt');
+            this.socket.disconnect();
+        }
+        console.log('Connecting to socket');
+        this.socket = io('ws://localhost:3001/neon-timing?token=' + this.apiKey, {
+            transports: ['websocket'],
+            upgrade: false,
+            reconnectionDelayMax: 2000
+        });
+        this.socket.on("connect", () => {
+            const engine = this.socket.io.engine;
+            this.hostSupportedEvents = [];
+            console.log('Connection opened');
+
+            engine.on("close", (reason) => {
+                this.hostSupportedEvents = [];
+                console.log('Connection closed');
+            });
+        });
+        this.socket.on('host_event', message => {
+            console.log('received event from host', message, JSON.stringify(message).length);
+            if (message.cmd === 'handshake_init') {
+                if (message.protocol !== 'NT1') {
+                    console.log('Protocol is not valid.')
+                    this.socket.disconnect();
+                }
+                this.hostSupportedEvents = message.events;
+                this.sendClientEvent({
+                    cmd: 'handshake_ack',
+                    events: this.clientSupportedEvents,
+                    device: 'PC Demo',
+                    init_time: message.time
+                });
+            }
+        });
+    }
+
+    setApiKey(apiKey) {
+        localStorage.setItem('nt-api-key', apiKey);
+        this.apiKey = apiKey;
+        this.debounceAttemptConnection();
+    }
+
+    getApiKey() {
+        return localStorage.getItem('nt-api-key') ?? '';
+    }
+
+    getFakeDeviceTime() {
+        return Date.now() - this.fakeDeviceTimeOffset;
+    }
+
+    newGateEvent(transponder) {
+        this.sendClientEvent({
+            cmd: 'event',
+            evt: 'gate',
+            type: 'transponder_passed_gate',
+            transponder
+        });
+    }
+
+    sendClientEvent(event) {
+        if (!this.socket || !this.socket.connected) {
+            console.log('socket is not connected, cannot send message');
+            return;
+        }
+        if (event.evt && !this.hostSupportedEvents.includes(event.evt)) {
+            console.log('Host does not support ' + event.evt + ' events');
+            return;
+        }
+        const data = {
+            ...event,
+            time: this.getFakeDeviceTime(),
+            protocol: 'NT1',
+            did: this.did
+        };
+        console.log('sending client event', data, JSON.stringify(event).length);
+        this.socket.emit('client_event', data);
+    }
+}
+
+export default new SocketClient();
